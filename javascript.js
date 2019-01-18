8f1b49dfb196b5f2fa0fb1f47b84621fc09b8d89
Nikki Holland-Plum <mnhollandplum@gmail.com>
2019-01-17 21:45:00 -0700

    js logic works

---
+++ javascript.js
@@ -0,0 +1,23 @@
+function yourGuess() {
+    var guess = document.getElementById("guess").value;
+    var guesses = document.getElementById("output");
+    if (guess == numToGuess) {
+        guesses.value = guesses.value + "\r" + "You have guessed correctly! ("+guess+")";
+    } else if (guess > numToGuess) {
+        guesses.value = guesses.value + "\r" + "Your last guess was "+guess+" That is too high";
+    } else {
+        guesses.value = guesses.value + "\r" + "Your last guess was "+guess+" That is too low";
+    }
+}
+
+function generateNumberToGuess(confirmIt) {
+    var guesses = document.getElementById("output");
+    guesses.value = '';
+    numToGuess = Math.floor(Math.random()*100);
+    guesses.value = "New number generated.\n";
+}
+
+
+window.onload = function(){
+    generateNumberToGuess();
+}
