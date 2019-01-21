4470bc523e06cb3a0be96b2717feb989103f8098
Nikki Holland-Plum <mnhollandplum@gmail.com>
2019-01-21 09:37:20 -0700

    logic for phase 3 working.

--- javascript.js
+++ javascript.js
@@ -4,13 +4,15 @@ function yourGuess() {
     document.getElementById("resetButton").disabled = false;
     document.getElementById("clearButton").disabled = false;
     if (guess == numToGuess) {
-        guesses.value = guesses.value + "\r" + "BOOM! ("+guess+")";
+        alert("Boom " +guess+ "is correct! Press reset to play again" )
+        guesses.value = "Press the reset button to play again";
+        winAddition();
     } else if(isNaN(guess)) {
         alert("That is not a valid number");
         guess.value = '';
         return;
-    } else if(guess > 100 || guess < 0) {
-        alert(guess + " is not within the accepted range, please pick a number between 1 and 100");
+    } else if(guess > maxSet || guess < minSet) {
+        alert(guess + " is not within the accepted range, please pick a number between " + minSet + " and " + maxSet);
         guessField.value = '';
         return;
     } else if (guess > numToGuess) {
@@ -23,11 +25,32 @@ function yourGuess() {
 function generateNumberToGuess() {
     var guesses = document.getElementById("output");
     guesses.value = '';
-    numToGuess = Math.floor(Math.random()*100);
+    numToGuess = Math.floor(Math.random()* (max - min)) + min;
     guesses.value = "New number generated.\n";
 }
 
 
 window.onload = function(){
     generateNumberToGuess();
+}
+
+var minSet = parseInt(document.getElementById("min").value);
+var maxSet = parseInt(document.getElementById("max").value);
+numToGuess = Math.floor(Math.random()* (maxSet - minSet)) + minSet;
+var setButton = document.querySelector('#setRange');
+setButton.addEventListener('click', setRange);
+
+function setRange() {
+    minSet = Number(min.value);
+    maxSet = Number(max.value);
+    numToGuess = Math.floor(Math.random() * (maxSet - minSet + 1)) + minSet;
+    alert("The new range is between " + minSet + " and " + maxSet + "." + " Please submit your guess:");
+}
+
+function winAddition(){
+  minSet -= 10;
+  maxSet += 10;
+  document.getElementById("min").value = min;
+  document.getElementById("max").value = max;
+  numToGuess = randomNumber();
 }
